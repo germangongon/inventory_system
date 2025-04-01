726a2f22d3e99094fb65e098ed27e45dfc39240b
German <gonzalez.german10@gmail.com>
2025-04-01 16:47:19 +0200

    Initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1&nc3#+dpx@!&l=43j#=bdc_triqbjt7c7%4peoyt@xv=5tqbj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
